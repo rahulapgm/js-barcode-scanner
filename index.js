54accc448925e657ed0931554e9d96f46cc9858a
rahulapgm <rahulpgm@gmail.com>
2025-09-13 17:22:39 +0530

    Updated file name

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 export { default as BarcodeScanner } from "./utils/barcode-scanner-opt.js";
 
 // Export the React custom hook
-export { useBarcodeScanner } from "./lib/useBarcodeScanner.js";
+export { useBarcodeScanner } from "./lib/use-barcode-scanner.js";
 
 // Export the vanilla JavaScript scanner
 export { VanillaBarcodeScanner, createBarcodeScanner } from "./lib/vanilla-scanner.js";
 
 // Default export is the React hook (for backward compatibility)
-export { default } from "./lib/useBarcodeScanner.js";
+export { default } from "./lib/use-barcode-scanner.js";
